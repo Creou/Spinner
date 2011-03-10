61e7e18afb32fa1aa8ea32712c3d319913ba5032
Simon P Stevens <simonpstevens@gmail.com>
2011-03-10 19:41:14 +0000

    First commit

---
+++ Spinner/SpinnerViewModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Spinner
+{
+    public class SpinnerViewModel : DependencyObject
+    {
+        public static readonly DependencyProperty AngleProperty = DependencyProperty.Register("Angle", typeof(double), typeof(SpinnerViewModel), new UIPropertyMetadata(0d));
+        public static readonly DependencyProperty SpinnerCentreXProperty = DependencyProperty.Register("SpinnerCentreX", typeof(double), typeof(SpinnerViewModel), new UIPropertyMetadata(250d));
+        public static readonly DependencyProperty SpinnerCentreYProperty = DependencyProperty.Register("SpinnerCentreY", typeof(double), typeof(SpinnerViewModel), new UIPropertyMetadata(250d));
+        public static readonly DependencyProperty SpinnerWidthProperty = DependencyProperty.Register("SpinnerWidth", typeof(double), typeof(SpinnerViewModel), new UIPropertyMetadata(1d));
+        public static readonly DependencyProperty SpinnerHeightProperty = DependencyProperty.Register("SpinnerHeight", typeof(double), typeof(SpinnerViewModel), new UIPropertyMetadata(1d));
+
+        public static readonly DependencyProperty AngularMomentumProperty = DependencyProperty.Register("AngularMomentum", typeof(double), typeof(SpinnerViewModel), new UIPropertyMetadata(0d));
+
+        /// <summary>
+        /// Gets or sets the AngularMomentum.
+        /// </summary>
+        public double AngularMomentum
+        {
+            get { return (double)GetValue(AngularMomentumProperty); }
+            set { SetValue(AngularMomentumProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the Angle.
+        /// </summary>
+        public double Angle
+        {
+            get { return (double)GetValue(AngleProperty); }
+            set { SetValue(AngleProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the Angle.
+        /// </summary>
+        public double SpinnerCentreX
+        {
+            get { return (double)GetValue(SpinnerCentreXProperty); }
+            set { SetValue(SpinnerCentreXProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the Angle.
+        /// </summary>
+        public double SpinnerCentreY
+        {
+            get { return (double)GetValue(SpinnerCentreYProperty); }
+            set { SetValue(SpinnerCentreYProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the Angle.
+        /// </summary>
+        public double SpinnerWidth
+        {
+            get { return (double)GetValue(SpinnerWidthProperty); }
+            set { SetValue(SpinnerWidthProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the Angle.
+        /// </summary>
+        public double SpinnerHeight
+        {
+            get { return (double)GetValue(SpinnerHeightProperty); }
+            set { SetValue(SpinnerHeightProperty, value); }
+        }
+
+        private Task _currentSpinTask;
+        private bool _spinning;
+        internal void Spin()
+        {
+            _currentSpinTask = Task.Factory.StartNew(() =>
+            {
+                _spinning = true;
+                while (_spinning)
+                {
+                    Thread.Sleep(10);
+                    this.Dispatcher.Invoke(new Action(() =>
+                    {
+                        this.Angle += this.AngularMomentum;
+                        if (Math.Abs(this.AngularMomentum) > 20)
+                        {
+                            this.AngularMomentum = 20;
+                        }
+                        else if (Math.Abs(this.AngularMomentum) > 0.01)
+                        {
+                            if (this.AngularMomentum > 0)
+                            {
+                                this.AngularMomentum *= 0.99;// -= 0.1;
+                            }
+                            else
+                            {
+                                this.AngularMomentum *= 0.99; //+= 0.1;
+                            }
+                        }
+                        else
+                        {
+                            this.AngularMomentum = 0;
+                            _spinning = false;
+                        }
+                    }));
+                }
+            });
+        }
+
+        internal void StopSpin()
+        {
+            _spinning = false;
+        }
+    }
+}
---
+++ Spinner/SpinnerWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace Spinner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const String ViewModelProviderKey = "ViewModelProvider";
+
+        private SpinnerViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                Stream stream = File.Open("Background.jpg", FileMode.Open);
+                BitmapImage imgsrc = new BitmapImage();
+                imgsrc.BeginInit();
+                imgsrc.StreamSource = stream;
+                imgsrc.EndInit();
+
+                imgBackground.Source = imgsrc;
+            }
+            catch (IOException) { }
+
+            _viewModel = TryFindResource(ViewModelProviderKey) as SpinnerViewModel;
+        }
+
+        private double RadToDeg(double radians)
+        {
+            return radians * 180 / Math.PI;
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed || e.MiddleButton == MouseButtonState.Pressed || e.RightButton == MouseButtonState.Pressed)
+            {
+                Point mousePos = e.GetPosition((Canvas)sender);
+                double opp = mousePos.X - _viewModel.SpinnerCentreX;
+                double adj = _viewModel.SpinnerCentreY - mousePos.Y;
+
+                double angle =0;
+                if (adj > 0)
+                {
+                    double ratio = opp / adj;
+                    angle = RadToDeg(Math.Atan(ratio));
+                }
+                else
+                {
+                    double ratio = opp / adj;
+                    angle = RadToDeg(Math.Atan(ratio))+180;
+                }
+                _viewModel.StopSpin();
+                _viewModel.AngularMomentum = angle - _viewModel.Angle;
+                _viewModel.Angle = angle;
+            }
+        }
+
+        private void Canvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            _viewModel.SpinnerHeight = Math.Min(e.NewSize.Width, e.NewSize.Height) / 500;
+            _viewModel.SpinnerWidth = _viewModel.SpinnerHeight;
+            _viewModel.SpinnerCentreX = e.NewSize.Width / 2;
+            _viewModel.SpinnerCentreY = e.NewSize.Height / 2;
+        }
+
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            _viewModel.Spin();
+        }
+    }
+
+    
+}
